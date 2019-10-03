85821e87dc159af89ccc24388cda9ca0293f1fbf
ccsunnyfd <chzhyfd@126.com>
2019-10-03 14:02:41 +0800

    create basic router.js elements to display classic layout

---
+++ src/components/RenderRouterView.vue
@@ -0,0 +1,14 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script>
+export default {
+  name: "",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="" scoped></style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+// import RenderRouterView from "@/components/RenderRouterView";
+import NotFound from "@/views/404";
 
 Vue.use(Router);
 
@@ -9,18 +10,111 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: Home
+      path: "/user",
+      // component: RenderRouterView,
+      // component: { render: h => h("router-view") },
+      component: () =>
+        import(/* webpackChunkName: "layout" */ "./layouts/UserLayout"),
+      children: [
+        {
+          path: "/user",
+          redirect: "/user/login"
+        },
+        {
+          path: "/user/login",
+          name: "login",
+          component: () =>
+            import(/* webpackChunkName: "user" */ "./views/User/Login")
+        },
+        {
+          path: "/user/register",
+          name: "register",
+          component: () =>
+            import(/* webpackChunkName: "user" */ "./views/User/Register")
+        }
+      ]
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
+      path: "/",
       component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+        import(/* webpackChunkName: "layout" */ "./layouts/BasicLayout"),
+      children: [
+        // dashboard
+        {
+          path: "/",
+          redirect: "/dashboard/analysis"
+        },
+        {
+          path: "/dashboard",
+          name: "dashboard",
+          component: { render: h => h("router-view") },
+          children: [
+            {
+              path: "/dashboard/analysis",
+              name: "analysis",
+              component: () =>
+                import(
+                  /* webpackChunkName: "dashboard" */ "./views/Dashboard/Analysis"
+                )
+            }
+          ]
+        },
+        // form
+        {
+          path: "/form",
+          name: "form",
+          component: { render: h => h("router-view") },
+          children: [
+            {
+              path: "/form/basic-form",
+              name: "basicForm",
+              component: () =>
+                import(/* webpackChunkName: "form" */ "./views/Forms/BasicForm")
+            },
+            {
+              path: "/form/step-form",
+              name: "stepform",
+              component: () =>
+                import(/* webpackChunkName: "form" */ "./views/Forms/StepForm"),
+              children: [
+                {
+                  path: "/form/step-form",
+                  redirect: "/form/step-form/info"
+                },
+                {
+                  path: "/form/step-form/info",
+                  name: "info",
+                  component: () =>
+                    import(
+                      /* webpackChunkName: "form" */ "./views/Forms/StepForm/Step1"
+                    )
+                },
+                {
+                  path: "/form/step-form/confirm",
+                  name: "confirm",
+                  component: () =>
+                    import(
+                      /* webpackChunkName: "form" */ "./views/Forms/StepForm/Step2"
+                    )
+                },
+                {
+                  path: "/form/step-form/result",
+                  name: "result",
+                  component: () =>
+                    import(
+                      /* webpackChunkName: "form" */ "./views/Forms/StepForm/Step3"
+                    )
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    },
+    {
+      path: "*",
+      name: "404",
+      component: NotFound
     }
   ]
 });
