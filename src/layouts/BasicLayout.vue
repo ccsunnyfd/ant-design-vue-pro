bb5e534a012f7ae98bcaea8550f78dccdb42855d
ccsunnyfd <chzhyfd@126.com>
2019-10-03 17:07:01 +0800

    add BasicLayout and collapse sidemenu effect

--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -1,9 +1,27 @@
 <template>
   <div>
-    <Header />
-    <SiderMenu />
-    <router-view></router-view>
-    <Footer />
+    <a-layout id="components-layout-demo-side" style="min-height: 100vh">
+      <a-layout-sider :trigger="null" collapsible v-model="collapsed">
+        <div class="logo" />
+        <SiderMenu />
+      </a-layout-sider>
+      <a-layout>
+        <a-layout-header style="background: #fff; padding: 0">
+          <a-icon
+            class="trigger"
+            :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+            @click="collapsed = !collapsed"
+          ></a-icon>
+          <Header />
+        </a-layout-header>
+        <a-layout-content style="margin: 0 16px">
+          <router-view></router-view>
+        </a-layout-content>
+        <a-layout-footer style="text-align: center">
+          <Footer />
+        </a-layout-footer>
+      </a-layout>
+    </a-layout>
   </div>
 </template>
 
@@ -17,8 +35,27 @@ export default {
     Header,
     Footer,
     SiderMenu
+  },
+  data() {
+    return {
+      collapsed: false
+    };
   }
 };
 </script>
 
-<style lang="" scoped></style>
+<style scoped>
+.trigger {
+  padding: 0 20px;
+  line-height: 64px;
+  font-size: 20px;
+}
+.trigger:hover {
+  background: #eeeeee;
+}
+/* #components-layout-demo-side .logo {
+  height: 32px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px;
+} */
+</style>
