181f03b6851198ff291d39f3018d2b07222c746b
ccsunnyfd <chzhyfd@126.com>
2019-10-06 15:55:32 +0800

    implement custom directives authority control

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from "./router";
 import store from "./store";
 import { Button, Layout, Icon, Drawer, Radio, Menu } from "ant-design-vue";
 import Authorized from "@/components/Authorized";
+import Auth from "@/directives/auth";
 
 Vue.config.productionTip = false;
 
@@ -14,6 +15,7 @@ Vue.use(Drawer);
 Vue.use(Radio);
 Vue.use(Menu);
 Vue.component("Authorized", Authorized);
+Vue.use(Auth);
 
 new Vue({
   router,
