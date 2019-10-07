eb7cbc26e49b52fb5e072a29eb39b321e471a16a
ccsunnyfd <chzhyfd@126.com>
2019-10-07 19:38:13 +0800

    encapsulate axios request

--- src/main.js
+++ src/main.js
@@ -6,7 +6,10 @@ import { Button, Layout, Icon, Drawer, Radio, Menu } from "ant-design-vue";
 import Authorized from "@/components/Authorized";
 import Auth from "@/directives/auth";
 
+import request from "@/utils/request";
+
 Vue.config.productionTip = false;
+Vue.prototype.$request = request;
 
 Vue.use(Button);
 Vue.use(Layout);
--- src/views/Dashboard/Analysis.vue
+++ src/views/Dashboard/Analysis.vue
@@ -6,7 +6,6 @@
 
 <script>
 // import random from "lodash/random";
-import axios from "axios";
 import Chart from "@/components/Chart";
 export default {
   data() {
@@ -26,27 +25,29 @@ export default {
   },
   methods: {
     getChartData() {
-      axios
-        .get("/api/dashboard/chart", { params: { ID: 12345 } })
-        .then(response => {
-          this.chartOption = {
-            title: {
-              text: "ECharts 入门示例",
+      this.$request({
+        url: "/api/dashboard/chart",
+        method: "get",
+        params: { ID: 12345 },
+      }).then(response => {
+        this.chartOption = {
+          title: {
+            text: "ECharts 入门示例",
+          },
+          tooltip: {},
+          xAxis: {
+            data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+          },
+          yAxis: {},
+          series: [
+            {
+              name: "销量",
+              type: "bar",
+              data: response.data,
             },
-            tooltip: {},
-            xAxis: {
-              data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
-            },
-            yAxis: {},
-            series: [
-              {
-                name: "销量",
-                type: "bar",
-                data: response.data,
-              },
-            ],
-          };
-        });
+          ],
+        };
+      });
     },
   },
   beforeDestroy() {
